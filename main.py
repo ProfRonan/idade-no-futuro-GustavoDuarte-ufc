140aa9b9ee5fcba178c95ae20270af8dbeabd6c2
GustavoDuarte-ufc <gustavoduarte2804@gmail.com>
2023-03-25 12:45:02 -0300

    Modificado

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ idade_atual = int(input())
 ano_comp = int(input())
 nome = str(input())
 
-if ano_atual < ano_comp:
+if ano_atual < ano_comp and ano_atual > 0 and idade_atual > 0 and ano_comp > 0:
     print(nome+", no ano de",ano_comp,"você terá",ano_comp-ano_atual+idade_atual,"anos")
