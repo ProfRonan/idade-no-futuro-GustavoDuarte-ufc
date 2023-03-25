dd8ff3e70fc8aaf368c5d3965cf10a999d843274
GustavoDuarte-ufc <gustavoduarte2804@gmail.com>
2023-03-25 12:31:28 -0300

    Modificado

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ idade_atual = int(input())
 ano_comp = int(input())
 nome = str(input())
 
-print(nome + ", no ano de " + ano_comp + " você terá " + ano_atual-ano_comp+idade_atual + " anos")
+print(nome+", no ano de",ano_comp,"você terá",ano_comp-ano_atual+idade_atual,"anos")
